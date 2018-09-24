b878789365448f534cfe4d778e2112e46187293a
Akrama Mirza <akrama.mirza@edu.uwaterloo.ca>
2018-09-23 21:49:10 -0400

    Init

---
+++ app/src/main/java/com/akrama/ridecompare/CompareActivity.kt
@@ -0,0 +1,12 @@
+package com.akrama.ridecompare
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CompareActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compare)
+    }
+}

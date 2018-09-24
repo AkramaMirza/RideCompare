859723c9a54c5eceac082e4c16b5245d8a717093
Akrama Mirza <akrama.mirza@edu.uwaterloo.ca>
2018-09-24 17:11:52 -0400

    Remove unnecessary booleans from CompareViewState

--- app/src/main/java/com/akrama/ridecompare/ComparePresenter.kt
+++ app/src/main/java/com/akrama/ridecompare/ComparePresenter.kt
@@ -3,15 +3,7 @@ package com.akrama.ridecompare
 object ComparePresenter {
 
     fun toViewState(dataModel: CompareModel): CompareViewState {
-        val isPickupLocationSelected = dataModel.pickupLocation != null
-        val isDestinationSelected = dataModel.destination != null
-
         return CompareViewState(
-                showSetPickupLocationButton = !isPickupLocationSelected,
-                showSetDestinationButton = !isDestinationSelected,
-                showChangePickupLocationButton = isPickupLocationSelected,
-                showChangeDestinationButton = isDestinationSelected,
-
                 pickupLocation = dataModel.pickupLocation?.address?.toString(),
                 destination = dataModel.destination?.address?.toString()
         )
--- app/src/main/java/com/akrama/ridecompare/CompareViewState.kt
+++ app/src/main/java/com/akrama/ridecompare/CompareViewState.kt
@@ -1,11 +1,6 @@
 package com.akrama.ridecompare
 
 data class CompareViewState(
-        val showSetPickupLocationButton: Boolean = true,
-        val showSetDestinationButton: Boolean = true,
-        val showChangePickupLocationButton: Boolean = false,
-        val showChangeDestinationButton: Boolean = false,
-
         val pickupLocation: String? = null,
         val destination: String? = null
 )
